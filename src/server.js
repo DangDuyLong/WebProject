7fd0ec3d61568994b0eb81318a160d56f35631ca
Quyen_Dinh <22010342@st.phenikaa-uni.edu.vn>
2024-05-12 18:16:30 +0700

    'nodejs'

---
+++ src/server.js
@@ -0,0 +1,26 @@
+import bodyParser from "body-parser";
+import express from "express";
+import viewEngine from "./config/ViewEngine";
+import connectDB from './config/connectDB';
+import initWebRoutes from './route/wed';
+
+require('dotenv').config();
+
+let app = express();
+
+//config app
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+
+viewEngine(app);
+initWebRoutes(app);
+
+connectDB();
+
+let port = process.env.PORT || 8080;
+//Port == undefined => port = 
+app.listen(port, () => {
+
+    console.log("Backend Nodejs is running on the port: " + port)
+})
