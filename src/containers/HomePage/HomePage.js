e2d14422846a4ee6ee6dc3153284c4e15c4cb079
Quyen_Dinh <22010342@st.phenikaa-uni.edu.vn>
2024-05-21 15:49:30 +0700

    'tạo_sticky_header_thêm_2_section'

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -3,12 +3,16 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
+import HomeFooter from './HomeFooter';
 import HomeHeader from './HomeHeader';
+import './HomeHeader.scss';
 import './HomePage.scss';
+import About from './Section/About';
 import HandBook from './Section/HandBook';
 import MedicalFacility from './Section/MedicalFacility';
 import OutStandingDoctor from './Section/OutStandingDoctor';
 import Specialty from './Section/Specialty';
+
 class HomePage extends Component {
 
     render() {
@@ -32,6 +36,8 @@ class HomePage extends Component {
                     settings={settings} />
                 <HandBook
                     settings={settings} />
+                <About />
+                <HomeFooter />
 
             </div>
         );
