108060975e3c5eac95bed2cb49284271dce962d6
Quyen_Dinh <22010342@st.phenikaa-uni.edu.vn>
2024-06-06 20:35:39 +0700

    'debug'

--- src/containers/System/Doctor/ManageSchedule.js
+++ src/containers/System/Doctor/ManageSchedule.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from "react-redux";
-<<<<<<< HEAD
-import DatePicker, * as actions from "../../../components/Input/DatePicker";
+import Select from 'react-select';
+import DatePicker from '../../../components/Input/DatePicker';
+import * as actions from "../../../store/actions";
 import { LANGUAGES } from '../../../utils';
 import './ManageSchedule.scss';
 
@@ -13,7 +14,7 @@ class ManageSchedule extends Component {
 
         this.state = {
             listDoctors: [],
-            selectedDoctors: {},
+            selectedDoctor: {},
             currentDate: '',
             rangeTime: []
         }
@@ -24,43 +25,18 @@ class ManageSchedule extends Component {
     }
     componentDidUpdate(prevProps, prevState, snapshot) {
         if (prevProps.allDoctors !== this.props.allDoctors) {
-=======
-import './ManageSchedule.scss';
-import { FormattedMessage } from 'react-intl';
-import Select from 'react-select';
-import DatePicker, * as actions from "../../../components/Input/DatePicker";
-import moment from 'moment';
-import { divide, range } from 'lodash';
-import { render } from 'node-sass';
-import { LANGUAGES } from '../../../utils';
-import { fetchAllDoctors } from '../../../store/actions';
-import { act } from 'react';
-
-
-class ManageSchedule extends Component {
-    constructor(props){
-        super(props);
-
-        this.state ={
-            listDoctors : [],
-            selectedDoctors: {},
-            currentDate: '',
-            rangeTime : []
-        }
-    }
-    componentDidMount(){
-        this.props.fetchAllDoctors();
-        this.props.fetchAllScheduleTime();
-    }
-    componentDidUpdate(prevProps, prevState, snapshot){
-        if(prevProps.allDoctors !== this.props.allDoctors){
->>>>>>> de97cd27a8a5a78717e1e664753c131d26560063
             let dataSelect = this.buildDataInputSelect(this.props.allDoctors)
             this.setState({
                 listDoctors: dataSelect
             })
         }
+        if (prevProps.allScheduleTime !== this.props.allScheduleTime) {
+            this.setState({
+                rangeTime: this.props.allScheduleTime
+            })
+        }
     }
+
 
     buildDataInputSelect = (inputData) => {
         let result = [];
@@ -80,13 +56,8 @@ class ManageSchedule extends Component {
 
         return result;
     }
-
     handleChangeSelect = async (selectedOption) => {
-<<<<<<< HEAD
         this.setState({ selectedDoctor: selectedOption });
-=======
-        this.setState({ selectedDoctor : selectedOption});
->>>>>>> de97cd27a8a5a78717e1e664753c131d26560063
     }
 
     handleOnchangeDatePicker = (date) => {
@@ -96,29 +67,18 @@ class ManageSchedule extends Component {
     }
 
     render() {
-        console.log('Webproject check status: ', this.state)
-<<<<<<< HEAD
         let { rangeTime } = this.state;
         let { language } = this.props;
         return (
             <div className="manage-schedule-container">
                 <div className="m-s-title">
                     <FormattedMessage id="manage-schedule.title" />
-=======
-        let {rangeTime} = this.state;
-        let {language} = this.props;
-        return(
-            <div className="manage-schedule-container">
-                <div className="m-s-title">
-                    <FormattedMessage id="manage-schedule.title"/>
->>>>>>> de97cd27a8a5a78717e1e664753c131d26560063
                 </div>
                 <div className="container">
                     <div className="row">
-                        <div className="col-6 from-group">
-                            <label> <FormattedMessage id="manage-schedule.choose-doctor" /> </label>
-<<<<<<< HEAD
-                            <select
+                        <div className="col-6 form-group">
+                            <label> <FormattedMessage id="manage-schedule.choose-doctor" /></label>
+                            <Select
                                 value={this.state.selectedDoctor}
                                 onChange={this.handleChangeSelect}
                                 options={this.state.listDoctors}
@@ -131,52 +91,24 @@ class ManageSchedule extends Component {
                                 className="form-control"
                                 value={this.state.currentDate}
                                 minDate={new Date()}
-=======
-                            <select 
-                            value={this.state.selectedDoctor}
-                            onChange={this.handleChangeSelect}
-                            options={this.state.listDoctors}
-                            />
-                        </div>
-                        <div className="col-6 form-group">
-                            <label> <FormattedMessage id ="manage-schedule.choose-date"/></label>
-                            <DatePicker
-                                onChange={this.handleOnchangeDatePicker}
-                                className = "form-control"
-                                value={this.state.currentDate}
-                                minDate = {new Date()}
->>>>>>> de97cd27a8a5a78717e1e664753c131d26560063
                             />
                         </div>
                         <div className="col-12 pick-hour-container">
                             {rangeTime && rangeTime.length > 0 &&
-                                rangeTime.map(item, index) => {
-                                    return(
-<<<<<<< HEAD
-                            <button className="btn btn-schedule" key={index}>
-                                {language === LANGUAGES.VI ? item.valueVi : item.valueEn}
-                            </button>
-                            )
-                                }
+                                rangeTime.map((item, index) => {
+                                    return (
+                                        <button className="btn btn-schedule" key={index}>
+                                            {language === LANGUAGES.VI ? item.valueVi : item.valueEn}
+                                        </button>
+                                    )
+                                })
                             }
+
 
                         </div>
                         <div className="col-12">
                             <button className=" btn btn-primary btn-save-schedule">
                                 <FormattedMessage id="manage-schedule.save" />
-=======
-                                        <button className="btn btn-schedule" key={index}>
-                                            {language === LANGUAGES.VI ? item.valueVi : item.valueEn}
-                                        </button>
-                                    )
-                                }
-                            }
-    
-                        </div>
-                        <div className="col-12">
-                            <button className=" btn btn-primary btn-save-schedule">
-                                <FormattedMessage id ="manage-schedule.save"/>
->>>>>>> de97cd27a8a5a78717e1e664753c131d26560063
                             </button>
                         </div>
                     </div>
@@ -185,33 +117,24 @@ class ManageSchedule extends Component {
         );
     }
 }
- 
+
 
 
 const mapStateToProps = state => {
     return {
         isLoggedIn: state.user.isLoggedIn,
-<<<<<<< HEAD
         language: state.app.language,
         allDoctors: state.admin.allDoctors,
         allScheduleTime: state.admin.allScheduleTime,
-=======
-        language:state.app.language,
-        allDoctors: state.admin.allDoctors,
-        allScheduleTime:state.admin.allScheduleTime,
->>>>>>> de97cd27a8a5a78717e1e664753c131d26560063
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
         fetchAllDoctors: () => dispatch(actions.fetchAllDoctors()),
+
         fetchAllScheduleTime: () => dispatch(actions.fetchAllScheduleTime()),
-<<<<<<< HEAD
     };
-=======
-    }; 
->>>>>>> de97cd27a8a5a78717e1e664753c131d26560063
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ManageSchedule);
--- src/store/actions/adminActions.js
+++ src/store/actions/adminActions.js
@@ -272,7 +272,7 @@ export const fetchAllScheduleTime = () => {
             if (res && res.errCode === 0) {
                 dispatch({
                     type: actionTypes.FETCH_ALLCODE_SCHEDULE_TIME_SUCCESS,
-                    dataTIME: res.data
+                    dataTime: res.data
                 })
             } else {
                 dispatch({
@@ -280,7 +280,7 @@ export const fetchAllScheduleTime = () => {
                 })
             }
         } catch (e) {
-            console.log('FFETCH_ALLCODE_SCHEDULE_TIME_FAILDED: ', e)
+            console.log('FETCH_ALLCODE_SCHEDULE_TIME_FAILDED: ', e)
             dispatch({
                 type: actionTypes.FETCH_ALL_DOCTORS_FAILDED
             })
