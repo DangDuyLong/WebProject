e546d445e09a0ff8121fc8eae20c748bec1d2b20
DangDuyLong <22010378@st.phenikaa-uni.edu.vn>
2024-06-07 18:05:25 +0700

    schedule

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -1,5 +1,6 @@
-import _ from 'lodash';
+import _, { reject } from 'lodash';
 import db from "../models/index";
+import { where } from 'sequelize';
 require('dotenv').config();
 
 const MAX_NUMBER_SCHEDULE = process.env.MAX_NUMBER_SCHEDULE;
@@ -196,10 +197,40 @@ let bulkCreateSchedule = (data) => {
     })
 }
 
+let getScheduleByDate = (doctorId, date) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!doctorId || !date) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'Missing required parameter'
+                })
+            } else {
+                let dataSchedule = await db.schedule.findAll({
+                    where: {
+                        doctorId: doctorId,
+                        date: date
+                    },
+
+                })
+
+                if (!dataSchedule) dataSchedule = [];
+
+                resolve({
+                    errCode: 0,
+                    data: dataSchedule
+                })
+            }
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
 module.exports = {
     getTopDoctorHome: getTopDoctorHome,
     getAllDoctors: getAllDoctors,
     saveDetailInforDoctor: saveDetailInforDoctor,
     getDetailDoctorById: getDetailDoctorById,
-    bulkCreateSchedule: bulkCreateSchedule
+    bulkCreateSchedule: bulkCreateSchedule,
+    getScheduleByDate: getScheduleByDate,
 }
